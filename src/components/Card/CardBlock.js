6281bbc20fc2b64c763372ab78a523adafa0168d
VojinPopovic <vojinpopovic3@gmail.com>
2022-06-09 21:33:20 +0200

    custom hook updated

--- src/components/Card/CardBlock.js
+++ src/components/Card/CardBlock.js
@@ -6,12 +6,11 @@ import "./CardBlock.css";
 import { motion } from "framer-motion";
 import useOnScrollLeft from "../../useOnScrollLeft";
 import useOnScrollRight from "../../useOnScrollRight";
-import {useRef, useCallback} from "react"
+import { useCallback } from "react";
 
 function CardBlock() {
   const [inViewRef, animation] = useOnScrollLeft();
   const [inViewRef2, animations] = useOnScrollRight();
-  const ref = useRef();
 
   // Use `useCallback` so we don't recreate the function on each render - Could result in infinite loop
   const setRefs = useCallback(
@@ -33,18 +32,10 @@ function CardBlock() {
       </div>
       <Card className="top-card">
         <div className="top-card-text-container">
-          <motion.h2
-            animate={{ x: 0 }}
-            initial={{ x: 960 }}
-            transition={{ delay: 2.5, duration: 2 }}
-          >
+          <motion.h2>
             A <span>frontend</span> <br /> developer
           </motion.h2>
-          <motion.p
-            animate={{ x: 0 }}
-            initial={{ x: 960 }}
-            transition={{ delay: 2.5, duration: 2 }}
-          >
+          <motion.p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illo
             delectus ullam debitis rerum nam pariatur, quo odio dolore aperiam?
             Repudiandae officiis facere autem, minima nostrum vel deleniti esse,
@@ -54,37 +45,18 @@ function CardBlock() {
             voluptas quasi voluptatem consequatur.
           </motion.p>
         </div>
-        <svg
-          animate={{ x: 0 }}
-          initial={{ x: 960 }}
-          transition={{ delay: 2.5, duration: 2 }}
-          width="236"
-          height="202"
-          viewBox="0 0 236 202"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M210 107.315C224.692 128.248 240.774 153.384 233.644 169.881C226.608 186.192 196.361 193.864 173.216 196.702C150.258 199.633 134.308 197.917 114.898 199.364C95.4883 200.811 72.6191 205.422 57.5247 196.896C42.4303 188.37 35.018 166.427 22.6805 136.169C10.4367 105.725 -6.91876 66.8714 3.12884 39.4651C13.0827 12.2451 50.3458 -3.34138 82.6178 0.746525C114.61 4.92707 141.516 28.9684 161.621 49.355C181.819 69.5552 195.308 86.3809 210 107.315Z"
-            fill="#DA3636"
-          />
+        {/* prettier-ignore */}
+        <svg width="236" height="202" viewBox="0 0 236 202" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M210 107.315C224.692 128.248 240.774 153.384 233.644 169.881C226.608 186.192 196.361 193.864 173.216 196.702C150.258 199.633 134.308 197.917 114.898 199.364C95.4883 200.811 72.6191 205.422 57.5247 196.896C42.4303 188.37 35.018 166.427 22.6805 136.169C10.4367 105.725 -6.91876 66.8714 3.12884 39.4651C13.0827 12.2451 50.3458 -3.34138 82.6178 0.746525C114.61 4.92707 141.516 28.9684 161.621 49.355C181.819 69.5552 195.308 86.3809 210 107.315Z" fill="#DA3636"/>
         </svg>
         <motion.img src={CardImage} alt="CardImage" className="card-image" />
       </Card>
       <Card className="bottom-card">
         <div className="top-card-text-container">
-          <motion.h2
-            animate={{ x: 0 }}
-            initial={{ x: -960 }}
-            transition={{ delay: 4, duration: 2 }}
-          >
+          <motion.h2>
             Ne znam, sta da ovde jebiga <span>stavim</span> stavim
           </motion.h2>
-          <motion.p
-            animate={{ x: 0 }}
-            initial={{ x: -960 }}
-            transition={{ delay: 4, duration: 2 }}
-          >
+          <motion.p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illo
             delectus ullam debitis rerum nam pariatur, quo odio dolore aperiam?
             Repudiandae officiis facere autem, minima nostrum vel deleniti esse,
