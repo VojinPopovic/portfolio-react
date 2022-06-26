bee915cb85f89affb40f442c3af5f2213e65a91f
VojinPopovic <vojinpopovic3@gmail.com>
2022-06-26 20:37:15 +0200

    hamburger menu added

--- src/components/navbar/NavbarMobile.js
+++ src/components/navbar/NavbarMobile.js
@@ -1,13 +1,28 @@
 import "./NavBar.css";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import useOnScrollNav from "./useOnScrollNav";
 import { AiOutlineMenu } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
 import NavbarLinks from "./NavbarLinks";
 import ContactButton from "./ContactButton";
 import "./NavbarMobile.css";
 
 function NavbarMobile() {
   const [ref, animation] = useOnScrollNav();
+  const [open, setOpen] = useState(false);
+
+  const hamburger = (
+    <div className="hamburger-icon-container">
+      <AiOutlineMenu color="white" size="50%" onClick={() => setOpen(!open)} />
+    </div>
+  );
+
+  const close = (
+    <div className="close-icon-container">
+      <MdClose color="white" size="50%" onClick={() => setOpen(!open)} />
+    </div>
+  );
 
   return (
     <div ref={ref}>
@@ -17,12 +32,8 @@ function NavbarMobile() {
         transition={{ duration: 1 }}
         className="navbar-container-mobile"
       >
-        <div className="hamburger-icon-container">
-            <AiOutlineMenu color="white" size="50%"/>
-        </div>
-        <div className="mobile-menu">
-          <NavbarLinks />
-        </div>
+        {open ? hamburger : close}
+        {open || <NavbarLinks />}
         <ContactButton />
       </motion.div>
     </div>
